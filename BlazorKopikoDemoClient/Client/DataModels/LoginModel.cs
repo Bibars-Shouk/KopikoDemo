40a3e0f03757e831ce7c30dab633470ffad868aa
Bibars-Shouk <bibars.shouk1@gmail.com>
2023-05-08 02:12:21 +0300

    Create login and sign up data models and replace html form and inputs with blazor forms and input components.

---
+++ BlazorKopikoDemoClient/Client/DataModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorKopikoDemoClient.Client.DataModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Must be at least 6 characters long.")]
+        public string Password { get; set; }
+    }
+}
---
+++ BlazorKopikoDemoClient/Client/DataModels/SignUpModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorKopikoDemoClient.Client.DataModels
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Must be at least 6 characters long.")]
+        public string Password { get; set; }
+    }
+}
